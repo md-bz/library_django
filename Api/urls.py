ba7c36b8e57921c33f634f1506caedf40ccaee27
mehrad <mmfirst@protonmail.com>
2025-08-16 14:57:44 +0330

    fix:fixed duplicate route names

--- Api/urls.py
+++ Api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("books/",views.index,name="index"),
-    path("books/<int:id>/",views.book_by_id,name="book_by_id"),    
-    path("books/<slug:slug>/",views.book,name="book"),
-    path("<path:path>/",views.not_found),
+    path("books/",views.index,name="api_index"),
+    path("books/<int:id>/",views.book_by_id,name="api_book_by_id"),    
+    path("books/<slug:slug>/",views.book,name="api_book"),
+    path("<path:path>/",views.not_found, name="api_not_found"),
 ]
